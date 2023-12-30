61fb9c074474ee46a5e583ab3fc850c1c4d27ac2
knowbits <bleken@gmail.com>
2023-12-31 00:06:58 +0100

    Updated the web scraping script: 1) More robust deletion of sections 2) More reobust insertion of the javascript 3) replaced Python web server with darkhttp Ditto updates in the main HTML file: added "section" markers to be used by "sed"

--- scraped_html__from_loaded_web_page/script__create_html_file__from_loaded_page.sh
+++ scraped_html__from_loaded_web_page/script__create_html_file__from_loaded_page.sh
@@ -15,23 +15,63 @@ if [ ! -d "$STATIC_HTML_FOLDER" ]; then
   exit 1
 fi
 
+# ====================================================================================================
+# "darkhttpd": Serve static files from a folder
+#
+# HOME: https://unix4lyfe.org/darkhttpd/
+# REPO: https://github.com/emikulic/darkhttpd
+#
+# darkhttpd is a simple, fast HTTP 1.1 web server for static content.
+# It doesn't do CGI, SSL, or anything else complicated.
+# It does what a web server should do: serve static content as fast as possible.
+# darkhttpd is a single small C file, no dependencies, no crap.
+#
+# STEPS: Install darkhttpd on Ubuntu
+#   $ sudo apt update && sudo apt -y upgrade
+#   $ sudo apt install build-essential
+#   $ git clone https://github.com/ryanmjacobs/darkhttpd
+#   $ cd darkhttpd
+#   $ make
+#   $ sudo mv darkhttpd /usr/local/bin
+#
+# Verify installation: $ darkhttpd
+
+# ====================================================================================================
+# Start the "darkhttpd" server to serve the static html page
+# The server will run in the background on: http://localhost:8080
+darkhttpd "$STATIC_HTML_FOLDER" --addr 127.0.0.1 --port 8000 &
+HTTP_PID=$!
+
+# USEFUL for DEBUGGING:
+# To check if the "darkhttpd" process is running:
+#    $ pgrep -f "darkhttpd . --addr 127.0.0.1 --port 8000"
+
+# Wait until the HTTP server is responding
+until curl -s http://127.0.0.1:8000 >/dev/null; do
+  echo
+  echo "Waiting for the 'darkhttpd' server to start..."
+  sleep 1
+done
+
+# ====================================================================================================
+# TODO: OBOSOLETE CODE: Python web server is no longer needed
+#
 # Start the python simple web server to serve the static html page
 # The server will run in the background on: http://localhost:8000
 # NOTE: The web server will run in the background
 #
-(
-  cd "$STATIC_HTML_FOLDER" && python3 -m http.server &
-  echo $! >"$SCRIPT_DIR/python_server.pid"
-
-  # Wait until the server is responding
-  until curl -s http://127.0.0.1:8000 >/dev/null; do
-    echo "Waiting for the Python server to start..."
-    sleep 1
-  done
-
-)
+# (
+#  cd "$STATIC_HTML_FOLDER" && python3 -m http.server &
+#  echo $! >"$SCRIPT_DIR/python_server.pid"
 #
+#  # Wait until the server is responding
+#  until curl -s http://127.0.0.1:8000 >/dev/null; do
+#    echo "Waiting for the Python server to start..."
+#    sleep 1
+#  done
+#)
 # => The PID is saved in the file: $SCRIPT_DIR/python_server.pid
+# ====================================================================================================
 
 # Set the CHROME_LOG_FILE environment variable
 export CHROME_LOG_FILE="$SCRIPT_DIR/chrome_debug.log"
@@ -48,25 +88,16 @@ chromium-browser \
   --dump-dom 'http://127.0.0.1:8000/radio_streams_player.html' \
   >"$SCRIPT_DIR"/radio_streams_player__scraped_from_loaded_page.html
 
-# Echo the result
-echo
-echo ============================================
-echo "Scraped the loaded web page (with innerHTML from JavaScript))."
-echo "Result was saved in: '$SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html'"
+# ====================================================================================================
+# Kill the "darkhttpd" server
+# Check if the web server process exists before killing it:
+if ps -p "$HTTP_PID" >/dev/null; then
+  kill -9 "$HTTP_PID"
+fi
 
-# Instructions to view the file
-echo
-echo "To view the resulting html file, run: "
-echo "   $ open $SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html"
-echo ============================================
-echo
-
-# Instructions to search the Chromium log file for "ERROR:" or "WARNING:"
-echo ============================================
-echo "To search the log file for error or warning, run: "
-echo "   grep -E 'ERROR:|WARNING:' $SCRIPT_DIR/chrome_debug.log"
-echo ============================================
-
+# ====================================================================================================
+# TODO: OBOSOLETE CODE: Python web server is no longer needed
+kill -9 "$HTTP_PID"
 # Check if the python web server process exists before killing it
 if [ -f "$SCRIPT_DIR/python_server.pid" ]; then
   PID=$(cat "$SCRIPT_DIR/python_server.pid")
@@ -75,29 +106,29 @@ if [ -f "$SCRIPT_DIR/python_server.pid" ]; then
   fi
   rm "$SCRIPT_DIR/python_server.pid"
 fi
+# ====================================================================================================
 
-# --------------------------------------------------
-# Now remove the parts of the JavaScript that are not needed for the static page
+# ====================================================================================================
+# Now remove the parts of the JavaScript that cannot be part of the "scraped" version of the loaded page
 
-!!! TODO: "radio_streams_player.html" has changel = that the "sed" commands below still works: !!! >Verify
+# Remove the "streams" array and preceding comment block:
+#
+sed -i '/\/\* SECTION_TO_REMOVE_1__START \*\//,/\/\* SECTION_TO_REMOVE_1__END \*\//c\/* for loop removed by the "web scraping" shell script */' \
+  "$SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html"
 
-# Remove the comment block preceding the "streams" array
-sed -i '/\/\* START:/,/END: \*\//d' "$SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html"
+# Remove the "    streams.forEach(..." loop, and replace it with the string "INSERT_NEW_SCRIPT_HERE"
+#
+sed -i '/\/\* SECTION_TO_REMOVE_2__START \*\//,/\/\* SECTION_TO_REMOVE_2__END \*\//c\INSERT_NEW_SCRIPT_HERE' \
+  "$SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html"
 
-# Remove the "streams" array
-sed -i '/const streams = \[/,/];/d' "$SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html"
+# ====================================================================================================
+# Now replace the string "INSERT_NEW_SCRIPT_HERE" with a minmal Javascript (adds an "event handler" to all buttons)
+# that adds event handlers to all the "audio stream buttons" on the scraped HTML page:
 
-# Remove the "streams.forEach" loop, and replace it with the string "INSERT_SCRIPT_HERE"
-sed -i '/streams.forEach(stream => {/,/});/c\INSERT_SCRIPT_HERE' "$SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html"
-
-# --------------------------------------------------
-# Now replace the string "INSERT_SCRIPT_HERE" with a Javascript that adds
-# event handlers to all the "audio stream buttons" on the scraped HTML page:
-
-# --------------------------------------------------
-# Now remove the parts of the JavaScript that are not needed for the static page
-
-cat <<'EOF' >script_block.txt
+# STEP 1: Keep the "new" Javascript in a variable
+#
+NEW_SCRIPT=$(
+  cat <<'EOF'
       // Get all buttons
       const buttons = document.getElementsByTagName('button');
 
@@ -116,12 +147,44 @@ cat <<'EOF' >script_block.txt
         }
       }
 EOF
+)
 
-# Then, use sed to replace "INSERT_SCRIPT_HERE" with the contents of script_block.txt:
-sed -i '/INSERT_SCRIPT_HERE/{
-    s/INSERT_SCRIPT_HERE//g
-    r script_block.txt
-}' "$SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html"
+# Save the script into a variable.
+# The "awk" converts the multi-line string into a single-line string where newline characters are represented as \\n.
+#
+NEW_SCRIPT_SINGLE_LINE=$(echo "$NEW_SCRIPT" | awk '{ printf "%s\\n", $0 }' ORS='')
 
-# Remove the temporary file
-rm script_block.txt
+# Use sed to replace the placeholder with the new script
+#
+sed -i "s|INSERT_NEW_SCRIPT_HERE|$NEW_SCRIPT_SINGLE_LINE|g" "$SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html"
+
+# ====================================================================================================
+# Pretty print the resulting HTML file using "tidy".
+#
+# ABOUT: https://www.html-tidy.org/
+#        Check, correct, and pretty-print HTML(5) files
+#
+# NOTE: TO install "tidy" on Ubuntu: "$ sudo apt-get install tidy"
+#
+tidy -i -w 120 -m "$SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html"
+
+# ====================================================================================================
+# Echo the result: Location of the final scraped HTML file
+echo
+echo ============================================
+echo "Scraped the loaded web page (with innerHTML from JavaScript))."
+echo "Result was saved in: '$SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html'"
+
+# Instructions to view the file
+echo
+echo "To view the resulting html file, run: "
+echo "   $ open $SCRIPT_DIR/radio_streams_player__scraped_from_loaded_page.html"
+echo ============================================
+echo
+
+# JUST IN CASE: Instructions to search the Chromium log file for "ERROR:" or "WARNING:"
+echo ============================================
+echo "To search the log file for error or warning, run: "
+echo "   grep -E 'ERROR:|WARNING:' $SCRIPT_DIR/chrome_debug.log"
+echo ============================================
+echo
